40b9644403a8b0b271442fb5f2fadc927329f061
johngreen-uta <johnjgreen@mavs.uta.edu>
2016-09-30 22:18:16 -0500

    Initial commit

---
+++ app/src/main/java/com/team11/cse4322/redalertuvnotifications/MainActivity.java
@@ -0,0 +1,13 @@
+package com.team11.cse4322.redalertuvnotifications;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
